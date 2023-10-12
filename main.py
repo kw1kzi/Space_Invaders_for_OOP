3401e20ebb406a820830cdd3ae589097d1c4b78b
Blowsight71345 <idiot71345@gmail.com>
2023-10-12 22:25:59 +0300

    add: Class MainCharacter and func start_game update: change ReadMe

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import pygame
 import sys
 
+
+def start_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1000, 800))
+    pygame.display.set_caption("SpaxeX by Pushkova")
+
+    flag = True
+    while flag:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.display.flip()
+
+
+start_game()
