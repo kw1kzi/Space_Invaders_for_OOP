a969c460d972569cc37fcdcb22beebcf12c79095
kw1kzi <danilkarachevtsev1702@gmail.com>
2023-12-17 13:05:59 +0300

    karachevtsev 23/2

--- main.py
+++ main.py
@@ -1,37 +1,162 @@
+import math
+import random
+
 import pygame
-import sys
-from hero import MainCharacter
+from pygame import mixer
 
-def start_game():
-    pygame.init()
-    screen = pygame.display.set_mode((1000,800))
-    pygame.display.set_caption("SpaceX by Karachevtsev")
-    maincharacter = MainCharacter()
+pygame.init()
 
-    flag = True
-    while flag:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_d:
-                    maincharacter.rect.centerx += 1
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_d:
-                    maincharacter.move_right = True
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_d:
-                    maincharacter.move_right = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    maincharacter.rect.centerx -= 1
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    maincharacter.move_left = True
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_a:
-                    maincharacter.move_left = False
+screen = pygame.display.set_mode((800, 600))
 
-        maincharacter.output()
-        pygame.display.flip()
-start_game()
+# Фон
+background = pygame.image.load('Images/background.jpg')
+
+# Название и иконка
+pygame.display.set_caption("Space Invaders")
+# Игрок
+playerImg = pygame.image.load('Images/hero.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+# Враги
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('Images/enemy.png'))
+    enemyX.append(random.randint(0, 736))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(4)
+    enemyY_change.append(40)
+
+# Пуля
+bulletImg = pygame.image.load('Images/bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 10
+bullet_state = "ready"
+
+# Счет
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+textX = 10
+testY = 10
+
+# Конец игры
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+
+def show_score(x, y):
+    score = font.render("Score: " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
+
+def game_over_text():
+    over_text = over_font.render("END", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
+
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
+
+
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
+    if distance < 27:
+        return True
+    else:
+        return False
+
+
+# Игровой цикл
+running = True
+while running:
+
+    screen.fill((0, 0, 0))
+    # background
+    screen.blit(background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Проверка клавиш
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -5
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 5
+            if event.key == pygame.K_SPACE:
+                if bullet_state is "ready":
+                    # точная координата
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    # Игрок
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    # Движение врагов
+    for i in range(num_of_enemies):
+
+        # Конец игры
+        if enemyY[i] > 440:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000
+            game_over_text()
+            break
+
+        enemyX[i] += enemyX_change[i]
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 4
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 736:
+            enemyX_change[i] = -4
+            enemyY[i] += enemyY_change[i]
+
+        # Столкновение
+        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+        if collision:
+            bulletY = 480
+            bullet_state = "ready"
+            score_value += 1
+            enemyX[i] = random.randint(0, 736)
+            enemyY[i] = random.randint(50, 150)
+
+        enemy(enemyX[i], enemyY[i], i)
+
+    # Движение пули
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+
+    if bullet_state is "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
+
+    player(playerX, playerY)
+    show_score(textX, testY)
+    pygame.display.update()
